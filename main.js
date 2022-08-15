2e8c423ef5ae0184f123ae93e65621d2017b0f65
Dev-Shifat <shahriar.ahmed.7878@gmail.com>
2022-08-15 16:29:02 +0600

    JS Commented

--- main.js
+++ main.js
@@ -4,36 +4,36 @@
  */
 // document.querySelectorAll('h2').style.color = "#ff0000";
 //problem-1
-const h2 = document.getElementsByTagName("h2");
-for (const all of h2) {
-    all.style.color = "lightblue";
-}
-//problem-2
-const backpack = document.getElementById("backpacks");
-backpack.style.backgroundColor = 'tomato';
-//problem-3
-const cards = document.getElementsByClassName('card');
-for (const card of cards) {
-    card.style.borderRadius = '30px';
-}
-function clickBy() {
-    console.log("Fuck Off");
-}
+// const h2 = document.getElementsByTagName("h2");
+// for (const all of h2) {
+//     all.style.color = "lightblue";
+// }
+// //problem-2
+// const backpack = document.getElementById("backpacks");
+// backpack.style.backgroundColor = 'tomato';
+// //problem-3
+// const cards = document.getElementsByClassName('card');
+// for (const card of cards) {
+//     card.style.borderRadius = '30px';
+// }
+// function clickBy() {
+//     console.log("Fuck Off");
+// }
 
-const btnHide = document.querySelectorAll("#buy-now-simple");
-for (const btn of btnHide) {
-    btn.addEventListener('click', function () {
-        btn.style.display = "none";
-    })
-}
+// const btnHide = document.querySelectorAll("#buy-now-simple");
+// for (const btn of btnHide) {
+//     btn.addEventListener('click', function () {
+//         btn.style.display = "none";
+//     })
+// }
 
-const inputHandeler = document.getElementById('exampleInputEmail1');
-inputHandeler.addEventListener('keyup', function (event) {
-    const text = event.target.value;
-    const right = document.getElementById('enable')
-    if (text === "mail") {
-        right.removeAttribute('disabled')
-    } else {
-        right.setAttribute("disabled", true)
-    }
-})
+// const inputHandeler = document.getElementById('exampleInputEmail1');
+// inputHandeler.addEventListener('keyup', function (event) {
+//     const text = event.target.value;
+//     const right = document.getElementById('enable')
+//     if (text === "mail") {
+//         right.removeAttribute('disabled')
+//     } else {
+//         right.setAttribute("disabled", true)
+//     }
+// })
